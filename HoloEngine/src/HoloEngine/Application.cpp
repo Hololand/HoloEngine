5d687210796c70fbb070030be3b66e51c393d23b
Holland <hololand@gmail.com>
2022-01-01 20:27:34 +0000

    Window Events

--- HoloEngine/src/HoloEngine/Application.cpp
+++ HoloEngine/src/HoloEngine/Application.cpp
@@ -1,21 +1,31 @@
 #include "hepch.h"
 #include "Application.h"
 
-#include "HoloEngine/Events/ApplicationEvent.h"
 #include "HoloEngine/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace HoloEngine {
 
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
 	HoloEngine::Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
 	}
 
 	HoloEngine::Application::~Application()
 	{
 	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		HE_CORE_TRACE("{0}", e);
+	}
+
 	void Application::Run()
 	{
 		while (m_Running)
@@ -25,6 +35,13 @@ namespace HoloEngine {
 			m_Window->OnUpdate();
 		}
 	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
+
 }
 
 
